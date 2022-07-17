a91df1844ba7af8752bb06d961929a56f0cc9fb0
Odenigbo Godwin <godwintrav@gmail.com>
2022-07-17 09:49:18 +0000

    exported migration file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-const ItemManager = artifacts.require("./ItemManager.sol");
+const ItemManager = artifacts.require("./ItemManager.so");
 
 module.exports = function (deployer) {
   deployer.deploy(ItemManager);
